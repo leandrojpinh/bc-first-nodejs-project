fc15e989eeb4f957a837fababe56ea7a92c2be5b
Leandro <leandro-jpinh@hotmail.com>
2020-04-21 23:27:57 -0300

    master - Desenvolvimento do desafio de transações e balanço.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,10 @@
 import Transaction from '../models/Transaction';
+import Balance from '../models/Balance';
 
-interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +15,29 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.all()
+      .filter(f => f.type === 'income')
+      .reduce((soma, current) => soma + current.value, 0);
+
+    const outcome = this.all()
+      .filter(f => f.type === 'outcome')
+      .reduce((soma, current) => soma + current.value, 0);
+
+    const balance = new Balance({ income, outcome });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
